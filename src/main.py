9be84152480da6ef604c0d8ff9db80b4c6b5e183
lucas.felico <lucasfeliccio@gmail.com>
2023-11-04 18:23:23 -0300

    chore: Remove unnecessary files

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
-from etl import pipeline
+'''
+Pipeline principal do projeto.
+'''
+from etl import extract, trasnform, load
+
+def etl_pipeline(data_base):
+    '''
+    Função principal do Pipeline ETL.
+    '''
+    try:
+        response = extract.extract_data(data_base)
+        if response == 200:
+            df = trasnform.transform_data(data_base)
+            load.load_data(df, data_base)
+            print('Pipeline ETL concluído com sucesso')
+        else:
+            print('A requisição HTTP retornou o seguinte status: {}'.format(response))
+    except Exception as e:
+        print('Erro ao realizar o Pipeline ETL -> {}'.format(e))
 
 if __name__ == '__main__':
-    pipeline.etl_pipeline('202301')
+    etl_pipeline('202301')
